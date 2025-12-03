8daa917ace6696c591d4fd294e5c3c087c5eb4f5
emmanuelgwatinyanya4-eng <emmanuelgwatinyanya4@gmail.com>
2025-12-03 17:21:18 +0530

    Add files via upload

---
+++ switch con.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int marks=90
+	switch (A>85)
+	{
+		case1: marks<80:
+			printf("case 1 is excecuted");
+			break;
+		case 2: marks>90:
+			printf("case 2 is excecuted");
+			break;
+		default:
+		printf("no cases are excecuted");	
+	}
+}
+
+
