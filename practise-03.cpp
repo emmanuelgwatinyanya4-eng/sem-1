d03b97d2b766aa8c6f650360ee284a7bd3c58876
emmanuelgwatinyanya4-eng <emmanuelgwatinyanya4@gmail.com>
2025-12-03 17:03:43 +0530

    Add files via upload

---
+++ practise-03.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int months = 12;
+	if(months > 12)
+	{
+		printf("Fake year");
+	}
+	else if(months < 12)
+	{
+		printf("Fake year");
+	}
+	else if(months =! 20)
+	{
+		printf("imperfect calendar year");
+	}
+	else
+	{
+		printf("Perfect calendar year");
+	}
+}
