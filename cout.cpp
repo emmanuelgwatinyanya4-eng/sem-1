cd5d687ef092f2f0be58275eee7f6f2d1dfcdd31
emmanuelgwatinyanya4-eng <emmanuelgwatinyanya4@gmail.com>
2025-12-03 13:47:38 +0530

    Add files via upload

---
+++ cout.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int A=2;
+    switch(A)
+	{
+		caseA:
+			printf("Two");
+			break;
+	    caseB:
+	    	printf("Two");
+	    	break;
+	    caseC:
+	    	printf("Three");
+	    	break;
+	    default:
+	    	printf("Else");
+	    	break;
+	}
+}
