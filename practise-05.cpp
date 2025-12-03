94e87e1e82f7d07ff8d51da3ef4b86cfd81a4170
emmanuelgwatinyanya4-eng <emmanuelgwatinyanya4@gmail.com>
2025-12-03 17:11:15 +0530

    Add files via upload

---
+++ practise-05.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int month=12;
+	switch(month)
+	{
+		case 1:
+			printf("January");
+			break;
+			
+			case 2:
+				printf("February");
+				break;
+				
+				case 3:
+					printf("March");
+					break;
+					
+					default:
+						printf("NO EXECUTABLE CASES");
+						break;
+	}
+	return 0;
+}
