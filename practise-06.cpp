fc4d35c914259485d76064bbb901ecf63ee278f4
emmanuelgwatinyanya4-eng <emmanuelgwatinyanya4@gmail.com>
2025-12-03 17:11:57 +0530

    Add files via upload

---
+++ practise-06.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int x= 15;
+	int y= 25;
+	if(x>11)
+	{
+		printf("X is greater than 11:\n");
+		if(y>19)
+		{
+			printf("Y is greater than 19");
+		}
+		return 0;
+	}
+}
