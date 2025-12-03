c72921011405ab31c2a1ff6bfe4307de43e93626
emmanuelgwatinyanya4-eng <emmanuelgwatinyanya4@gmail.com>
2025-12-03 13:50:57 +0530

    Add files via upload

---
+++ else if ladder.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+	int month;
+	printf("Enter the month (1-12):");
+	scanf("%d",&month);
+		if(month==1)
+		{
+			printf("It is Jnauary");
+		}
+		else if(month==2)
+		{
+			printf("It is February");
+		}
+		else if(month==3)
+		{
+			printf("It is March");
+		}
+		else if(month==4)
+		{
+			printf("It is April");
+		}
+		else if (month==5)
+		{
+			printf("It is May");
+		}
+		else if(month==6)
+		{
+			printf("It is June");
+		}
+		else
+		{
+			printf("no month");
+		}
+	}
