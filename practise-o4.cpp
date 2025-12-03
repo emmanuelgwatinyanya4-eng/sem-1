0f5d6523a0b10561ebd38bce438404cf9737f3b5
emmanuelgwatinyanya4-eng <emmanuelgwatinyanya4@gmail.com>
2025-12-03 17:12:56 +0530

    Add files via upload

---
+++ practise-o4.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int number=2;
+	switch(number)
+	{
+		case 1:
+			printf("Numer is 1");
+			break;
+		case 2:
+		printf("Number is 2");
+		break;
+		default:
+		printf("Number is not 1 or 2");	
+	}
+	return 0;
+}
+	
