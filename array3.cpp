c958b15be7a7cafc528ffc7d3f39ad0ece11f1c5
emmanuelgwatinyanya4-eng <emmanuelgwatinyanya4@gmail.com>
2025-12-03 13:44:55 +0530

    Add files via upload

---
+++ array3.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int i;int Roll_no[5];
+	printf("Enter Roll_no");
+	for(i=0;i<5;i++)
+	{
+		scanf;'{'"%d",&Roll_no[1]};
+	}
+	for(i=0;i<5;i++);
+	{
+		printf("%d",Roll_no[1])
+	}
+}
